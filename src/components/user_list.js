80cd77da2f126e887937380dfda5ac144a73378b
Rishi Karri <rishikarri1@gmail.com>
2017-07-05 14:23:38 -0400

    modified user list

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class UserList extends Component {
+	renderUser(user) {
+		return (
+			<div className="card card-block">
+				<h4 className="card-title">{user.name}</h4>
+				<p className='card-text'>Cheese Factory</p>
+				<a className='btn btn-primary'>Email</a>
+			</div>
+		)
+	}
 	render() {
 		return (
-			
+			<div>
+				{this.props.users.map(this.renderUser)}
+			</div>
 		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		users: state.users
 	}
 }
