097f20d6f89cedab6be908510d39781fd1118b67
Rishi Karri <rishikarri1@gmail.com>
2017-07-12 17:03:05 -0400

    building case for async middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,12 @@
 export default function({ dispatch }){
 	return next => action => {
-		console.log(action);
-
-		next(action);
+		// if the action does not have a payload or the payload does not have a .then property, we don't care about it, send it on			
+		if (!action.payload || !action.payload.then) {
+			// we know it is not a promise so we don't care about it, send it onward 
+			return next(action);
+		}
+		console.log('we dont have a promise', action);
 	};	
 }
+
+// .then is a promise helper 
