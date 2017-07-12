c9b8fb5388739704d74c090692c8d55f199e789e
Rishi Karri <rishikarri1@gmail.com>
2017-07-12 17:24:52 -0400

    updated reducers accordingly

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -5,7 +5,8 @@ import {
 export default function (state = [], action) {
 	switch (action.type) {
 		case FETCH_USERS: 
-			return [ ...state, ...action.payload ];
+			console.log(action.payload);
+			return [ ...state, ...action.payload.data ];
 	}
 	return state;
 }
