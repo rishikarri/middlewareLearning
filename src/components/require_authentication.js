2ea1b8dbae0fa3d2afdf63ebdc8086ddb08955c9
Rishi Karri <rishikarri1@gmail.com>
2017-07-28 14:19:50 -0400

    learning HOC

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -11,3 +11,20 @@ export default function(ComposedComponent) {
 
 	return Authentication;
 }
+
+// In some other location... not in this file...
+// We want to use this HOC 
+// How do we make use of this thing? 
+
+import Authentication // this is my HOC 
+import Resources //this is the component that i want to wrap 
+
+// I now have my higher order component and the component I want to wrap 
+
+const ComposedComponent = Authentication(Resources);
+
+in some render method, write somethings like 
+
+<ComposedComponent  />
+
+// this is the the enhanced componenet
