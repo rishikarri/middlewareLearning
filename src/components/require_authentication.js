fe026412c6640c06380be6c8a41519d4ef88aa94
Rishi Karri <rishikarri1@gmail.com>
2017-07-31 16:35:20 -0400

    added lifecycle methods

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -12,8 +12,17 @@ export default function(ComposedComponent) {
 		}
 
 		componentWillMount() {
-			this.context.router.push('/');
+			if(!this.props.authenticated) {
+				this.context.router.push('/');				
+			}
 		}
+
+		componentWillUpdate(nextProps) {
+			if(!this.props.authenticated) {
+				this.context.router.push('/');
+			}
+		}
+
 		render() {
 			console.log(this.context);
 
